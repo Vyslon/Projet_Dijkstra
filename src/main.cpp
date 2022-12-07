9e962f029081fe9185b21c78b0f5772f8590d62b
Vyslon <santoni.thomas26@hotmail.fr>
2022-12-07 20:37:45 +0100

    suppression de git de fichiers CLions

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main()
     {
         for (int j = 0; j < 3; j++)
         {
-            std::cout << tab[3 * i + j].distance << "  ";
+            std::cout << tab[3 * i + j].idPredecesseur << "  ";
         }
         std::cout << std::endl;
 
